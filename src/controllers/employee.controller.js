d9de870cbd7d2afe98e99bba3e460878a959edb9
Lazaro Mateus Mbaila <lazarozito@gmail.com>
2020-07-11 05:56:03 -0700

    rollback transaction

--- src/controllers/employee.controller.js
+++ src/controllers/employee.controller.js
@@ -8,7 +8,7 @@ module.exports = {
     const {name, surname, born_at, nr_document, gender, password, id_group_users} = req.body;
  
     try {
-      const trx  = knex.transaction();
+     
       const id_user = `${crypto.randomBytes(4).toString('Hex')}${name[0]}${surname[0]}`;
       const id_employee = `${crypto.randomBytes(4).toString('Hex')}`;
       const user_name = `${name}${id_user.substring(0, 4)}`;
@@ -16,16 +16,22 @@ module.exports = {
       const person = await knex('person').where({nr_document}).first();
 
       if(person) return res.status(400).send({error: 'user with same document number already exists'}); 
-      const id_person = await knex('person').insert({name, surname, born_at, nr_document, gender});
-      console.log(id_person);
-      await knex('users').insert({id_user, id_group_users, id_person, user_name, password:password_hash});
-
-      await knex('employee').insert({id_employee, id_user})
+      await knex.transaction(async trx => {
+        const id_person = 
+        await knex('person')
+        .insert({name, surname, born_at, nr_document, gender}, 'id_person').transacting(trx);
+       await knex('users')
+       .insert({id_user, id_group_users, id_person:id_person[0], user_name, password:password_hash})
+       .transacting(trx);
+       await knex('employee')
+       .insert({id_employee, id_user})
+       .transacting(trx);
+      });
+      
       return res.status(200).send({message: 'user created sucessfully'});
     }catch(err){
       console.log(err);
       return res.status(400).send({error: 'Registration failed'});
-
     }
   }
 }
